d7d9935eb74d70704a5a73edf12f3bbdfb6f4590
Dard001 <dard001@hotmail.com>
2021-12-09 18:37:15 -0800

    12/09/2021

--- includes/setChangePW.inc.php
+++ includes/setChangePW.inc.php
@@ -1,47 +1,50 @@
 <?php
-     if(isset($_POST['set-change-password'])){
+    session_start();
+    if (isset($_SESSION['userId'])){
+         if(isset($_POST['set-change-password'])){
 
-        $uid = $_POST['uid'];
+            $uid = $_POST['uid'];
 
-        if(empty($uid)){
-            header("Location: ../adminpanel.php?error=emptyfields");
-            exit();
-        } else {
-            require 'dbhandler.inc.php';
-            $DB = new dbHandler();
-            $conn = $DB->getDBConnection();
-            $DB = null;
-            
-            $sql_uid = "SELECT uid FROM accounts WHERE uid=?";
-            $stmt_uid = $conn->stmt_init();
-
-            if(!$stmt_uid->prepare($sql_uid)){
-                header("Location: ../adminpanel.php?error=PREPAREUIDsqlerror");
+            if(empty($uid)){
+                header("Location: ../adminpanel.php?error=emptyfields");
                 exit();
             } else {
-                $stmt_uid->bind_param("s", $uid);
-                $stmt_uid->execute();
-                $stmt_uid->store_result();
-                $resultCheckUid = $stmt_uid->num_rows();
+                require 'dbhandler.inc.php';
+                $DB = new dbHandler();
+                $conn = $DB->getDBConnection();
+                $DB = null;
+            
+                $sql_uid = "SELECT uid FROM accounts WHERE uid=?";
+                $stmt_uid = $conn->stmt_init();
 
-                if ($resultCheckUid == 0){
-                    header("Location: ../adminpanel.php?error=nouser=".$uid);
+                if(!$stmt_uid->prepare($sql_uid)){
+                    header("Location: ../adminpanel.php?error=PREPAREUIDsqlerror");
                     exit();
                 } else {
-                    $sql = "UPDATE accounts SET pwd_change=? WHERE uid=?";
-                    $stmt = $conn->stmt_init();
+                    $stmt_uid->bind_param("s", $uid);
+                    $stmt_uid->execute();
+                    $stmt_uid->store_result();
+                    $resultCheckUid = $stmt_uid->num_rows();
 
-                    if(!$stmt->prepare($sql)){
-                        header("Location: ../adminpanel.php?error=sqlerror");
-                        $conn->close();
-                        exit(); 
+                    if ($resultCheckUid == 0){
+                        header("Location: ../adminpanel.php?error=nouser=".$uid);
+                        exit();
                     } else {
-                        $boolean = 1;
-                        $stmt->bind_param("ss", $boolean, $uid);
-                        $stmt->execute();
-                        header("Location: ../adminpanel.php?info=setChangePW");
-                    }
-                }  
+                        $sql = "UPDATE accounts SET pwd_change=? WHERE uid=?";
+                        $stmt = $conn->stmt_init();
+
+                        if(!$stmt->prepare($sql)){
+                            header("Location: ../adminpanel.php?error=sqlerror");
+                            $conn->close();
+                            exit(); 
+                        } else {
+                            $boolean = 1;
+                            $stmt->bind_param("ss", $boolean, $uid);
+                            $stmt->execute();
+                            header("Location: ../adminpanel.php?info=setChangePW");
+                        }
+                    }  
+                }
             }
         }
     }
